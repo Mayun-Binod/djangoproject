1b0f75854a2786c98ff7a0e09f239d1b1535d5d7
Mayun-binod <shresthabinod334@gmail.com>
2024-09-27 18:08:49 +0545

    added

--- myapp/emp/urls.py
+++ myapp/emp/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("home/",emp_home),
-    path("add-emp/",add_emp),
-    
+    path("home/", emp_home, name='emp_home'),
+    path("add-emp/", add_emp, name='add_emp'),
+    path("delete-emp/<int:emp_id>/", delete_emp, name='delete_emp'),
+    path("update-emp/<int:emp_id>/", update_emp, name='update_emp'),
+    path("do-update-emp/<int:emp_id>/", do_update_emp, name='do_update_emp'),
 ]
--- myapp/emp/views.py
+++ myapp/emp/views.py
@@ -1,15 +1,94 @@
-from django.shortcuts import render, redirect
-from django.db import IntegrityError
+# from django.shortcuts import render,redirect
+# from django.http import HttpResponse
+# from .models import Emp
+# # Create your views here.
+# def emp_home(request):
+
+#     emps=Emp.objects.all()
+#     return render(request, "emp/home.html",{
+#         'emps':emps
+#     })
+
+# def add_emp(request):
+#     if request.method=="POST":
+        
+#         #data fetch
+#         emp_name=request.POST.get("emp_name")
+#         emp_id=request.POST.get("emp_id")
+#         emp_phone=request.POST.get("emp_phone")
+#         emp_address=request.POST.get("emp_address")
+#         emp_working=request.POST.get("emp_working")
+#         emp_department=request.POST.get("emp_department")
+
+#         #validate
+
+#         #create model object and set the data
+#         e=Emp()
+#         e.name=emp_name
+#         e.emp_id=emp_id
+#         e.phone=emp_phone
+#         e.address=emp_address
+#         e.department=emp_department
+#         if emp_working is None:
+#             e.working=False
+#         else:
+#             e.working=True
+#         #save the object
+#         e.save()
+#         #prepare msg
+        
+#         return redirect("/emp/home/")
+    
+#     return render(request, "emp/add_emp.html",{})
+
+
+# def delete_emp(request,emp_id):
+#     emp=Emp.objects.get(pk=emp_id)
+#     emp.delete()
+#     return redirect("/emp/home/")
+
+# def update_emp(request,emp_id):
+#     emp=Emp.objects.get(pk=emp_id)
+#     return render(request, "emp/update_emp.html",{
+#         'emp':emp
+#     })
+
+# def do_update_emp(request,emp_id):
+#     if request.method=='POST':
+#         emp_name=request.POST.get("emp_name")
+#         emp_id_temp=request.POST.get("emp_id")
+#         emp_phone=request.POST.get("emp_phone")
+#         emp_address=request.POST.get("emp_address")
+#         emp_working=request.POST.get("emp_working")
+#         emp_department=request.POST.get("emp_department")  
+    
+#         e=Emp.objects.get(pk=emp_id)
+#         e.name=emp_name
+#         e.emp_id=emp_id_temp
+#         e.phone=emp_phone
+#         e.address=emp_address
+#         e.department=emp_department
+#         if emp_working is None:
+#             e.working=False
+#         else:
+#             e.working=True
+
+#         e.save()
+#     return redirect("/emp/home/")
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from .models import Emp
 
-# Home page
+# Create your views here.
 def emp_home(request):
-    return render(request, 'emp/home.html', {})
+    emps = Emp.objects.all()
+    return render(request, "emp/home.html", {
+        'emps': emps
+    })
 
-# Add Employee
 def add_emp(request):
     if request.method == "POST":
-        # Data fetch from form
+        # Data fetch
         emp_name = request.POST.get("emp_name")
         emp_id = request.POST.get("emp_id")
         emp_phone = request.POST.get("emp_phone")
@@ -17,23 +96,52 @@ def add_emp(request):
         emp_working = request.POST.get("emp_working")
         emp_department = request.POST.get("emp_department")
 
-        # Create Emp model object
-        e = Emp(
-            name=emp_name,
-            emp_id=emp_id,
-            phone=emp_phone,
-            address=emp_address,
-            department=emp_department,
-            working=emp_working is not None  # Sets True if checkbox checked, False otherwise
-        )
+        # Validate required fields
+        if not emp_name or not emp_id:
+            return render(request, "emp/add_emp.html", {"error": "Name and ID are required."})
 
-        try:
-            # Save employee data
-            e.save()
-            return redirect("/emp/home/")
+        # Create model object and set the data
+        e = Emp()
+        e.name = emp_name
+        e.emp_id = emp_id
+        e.phone = emp_phone
+        e.address = emp_address
+        e.department = emp_department
+        e.working = bool(emp_working)  # Set to True if emp_working is checked, else False
 
-        except IntegrityError:
-            # Handle unique constraint failure for emp_id
-            return render(request, 'emp/add_emp.html', {'error': 'Employee ID already exists!'})
-    
-    return render(request, 'emp/add_emp.html', {})
+        # Save the object
+        e.save()
+        return redirect("/emp/home/")
+
+    return render(request, "emp/add_emp.html", {})
+
+def delete_emp(request, emp_id):
+    emp = get_object_or_404(Emp, pk=emp_id)  # Use get_object_or_404 to handle non-existent entries
+    emp.delete()
+    return redirect("/emp/home/")
+
+def update_emp(request, emp_id):
+    emp = get_object_or_404(Emp, pk=emp_id)  # Use get_object_or_404 here as well
+    return render(request, "emp/update_emp.html", {
+        'emp': emp
+    })
+
+def do_update_emp(request, emp_id):
+    if request.method == 'POST':
+        emp_name = request.POST.get("emp_name")
+        emp_id_temp = request.POST.get("emp_id")
+        emp_phone = request.POST.get("emp_phone")
+        emp_address = request.POST.get("emp_address")
+        emp_working = request.POST.get("emp_working")
+        emp_department = request.POST.get("emp_department")
+
+        e = get_object_or_404(Emp, pk=emp_id)  # Handle non-existent employee
+        e.name = emp_name
+        e.emp_id = emp_id_temp
+        e.phone = emp_phone
+        e.address = emp_address
+        e.department = emp_department
+        e.working = bool(emp_working)  # Set working status based on the presence of emp_working
+
+        e.save()
+    return redirect("/emp/home/")
